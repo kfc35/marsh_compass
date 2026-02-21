74eab03b575dcf4afacd6237e636d4e54fd9849f
Kevin Chen <chen.kevin.f@gmail.com>
2026-02-21 16:10:01 -0500

    Add bevy deps, stub example, state intention

---
+++ examples/simple_navigation.rs
@@ -0,0 +1,23 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Node {
+            width: percent(100),
+            height: percent(100),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        Text::new("Hello World"),
+        BackgroundColor(Color::Srgba(Srgba::RED)),
+    ));
+}
