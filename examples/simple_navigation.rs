aa4cd2201cf16695d940ee95b50a3e40a6e842fc
Kevin Chen <chen.kevin.f@gmail.com>
2026-02-21 19:18:34 -0500

    flesh out example more

--- examples/simple_navigation.rs
+++ examples/simple_navigation.rs
@@ -1,23 +1,66 @@
+use bevy::input_focus::{
+    InputDispatchPlugin, InputFocusVisible, directional_navigation::DirectionalNavigationPlugin,
+};
 use bevy::prelude::*;
+use bevy::ui::auto_directional_navigation::AutoDirectionalNavigation;
+use marsh_compass::AutoNavVizPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((
+            DefaultPlugins,
+            InputDispatchPlugin,
+            DirectionalNavigationPlugin,
+        ))
+        .add_plugins(AutoNavVizPlugin)
+        .insert_resource(InputFocusVisible(true))
         .add_systems(Startup, setup)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
-    commands.spawn((
-        Node {
+    let root_id = commands
+        .spawn((Node {
             width: percent(100),
             height: percent(100),
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
             ..default()
-        },
-        Text::new("Hello World"),
-        BackgroundColor(Color::Srgba(Srgba::RED)),
-    ));
+        },))
+        .id();
+
+    let positions = [(100, 100), (300, 100)];
+    for (left, top) in positions {
+        let button = spawn_button(&mut commands, left, top);
+        commands.entity(root_id).add_child(button);
+    }
+}
+
+fn spawn_button(commands: &mut Commands, left: i32, top: i32) -> Entity {
+    commands
+        .spawn((
+            Button,
+            Node {
+                position_type: PositionType::Absolute,
+                left: px(left),
+                top: px(top),
+                width: px(100),
+                height: px(50),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                border: UiRect::all(px(5)),
+                border_radius: BorderRadius::all(px(12)),
+                ..default()
+            },
+            BackgroundColor(Color::Srgba(Srgba::RED)),
+            // Just add this component for automatic navigation
+            AutoDirectionalNavigation::default(),
+        ))
+        .with_child((
+            Text::new("Hello"),
+            TextLayout {
+                justify: Justify::Center,
+                ..default()
+            },
+        ))
+        .id()
 }
